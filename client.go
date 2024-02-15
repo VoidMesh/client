88194f7820d019f097362ebcbd2785c2340f482e
Alyx <github@alyx.pink>
2024-02-15 15:18:33 +0100

    Add basic characters and resources interactions

--- client.go
+++ client.go
@@ -18,18 +18,60 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+	greeter := pb.NewGreeterClient(conn)
+	resource := pb.NewResourceServiceClient(conn)
+	character := pb.NewCharacterServiceClient(conn)
 
-	// Contact the server and print out its response.
-	name := "world"
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
+	var command, subcommand string
+	command = os.Args[1]
+	subcommand = os.Args[2]
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+
+	switch os.Args[1] {
+	case "hello":
+		name := os.Args[2]
+		r, err := greeter.SayHello(ctx, &pb.HelloRequest{Name: name})
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.GetMessage())
+	case "resource":
+		switch subcommand {
+		case "read":
+			name := os.Args[2]
+			r, err := resource.ReadResource(ctx, &pb.ReadResourceRequest{Resource: &pb.Resource{Name: name}})
+			if err != nil {
+				log.Fatalf("could not get resource: %v", err)
+			}
+			log.Printf("Resource: %s", r.GetResource())
+		case "list":
+			r, err := resource.ListResources(ctx, &pb.ListResourcesRequest{})
+			if err != nil {
+				log.Fatalf("could not list resources: %v", err)
+			}
+			log.Printf("Resources: %s", r.Resources)
+		}
+	case "character":
+		switch subcommand {
+		case "create":
+			name := os.Args[3]
+			r, err := character.CreateCharacter(ctx, &pb.CreateCharacterRequest{Character: &pb.Character{Name: name}})
+			if err != nil {
+				log.Fatalf("could not get character: %v", err)
+			}
+			log.Printf("Character: %s", r.GetCharacter())
+		case "read":
+			name := os.Args[3]
+			r, err := character.ReadCharacter(ctx, &pb.ReadCharacterRequest{Character: &pb.Character{Name: name}})
+			if err != nil {
+				log.Fatalf("could not get character: %v", err)
+			}
+			log.Printf("Character: %s", r.GetCharacter())
+		}
+	default:
+		log.Fatalf("Invalid command: %s", command)
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+
+	defer cancel()
 }
